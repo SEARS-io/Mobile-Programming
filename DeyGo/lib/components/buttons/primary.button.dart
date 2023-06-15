a8f61fefc582a24ebd56a894ffd573de05c1c175
Blanco237 <asongrandy9@gmail.com>
2023-06-15 23:25:12 +0100

    Refactored Components

--- DeyGo/lib/components/buttons/primary.button.dart
+++ DeyGo/lib/components/buttons/primary.button.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-Widget primaryButton(
-    {required String content,
-    required VoidCallback onPressed,
-    required BuildContext context,
-    bool disabled = false,
-    bool hasShadow = false}) {
-  return ElevatedButton(
-      onPressed: disabled ? null : onPressed,
-      style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.all(20),
-          textStyle: const TextStyle(fontSize: 17, fontWeight: FontWeight.w700),
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: Theme.of(context).colorScheme.secondary,
-          elevation: 10,
-          shadowColor: hasShadow ? Theme.of(context).primaryColor : null,
-          alignment: Alignment.center,
-          shape: const StadiumBorder(),
-          minimumSize: const Size.fromHeight(60),
-          disabledBackgroundColor: Theme.of(context).colorScheme.surface,
-          disabledForegroundColor: Theme.of(context).colorScheme.onSurface),
-      child: Text(content));
+// ignore: must_be_immutable
+class PrimaryButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String content;
+  bool disabled = false;
+  bool hasShadow = false;
+
+  PrimaryButton(
+      {super.key,
+      required this.onPressed,
+      required this.content,
+      this.disabled = false,
+      this.hasShadow = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: disabled ? null : onPressed,
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(20),
+            textStyle: GoogleFonts.urbanist(
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Theme.of(context).colorScheme.secondary,
+            elevation: 10,
+            shadowColor: hasShadow ? Theme.of(context).primaryColor : null,
+            alignment: Alignment.center,
+            shape: const StadiumBorder(),
+            minimumSize: const Size.fromHeight(60),
+            disabledBackgroundColor: Theme.of(context).colorScheme.surface,
+            disabledForegroundColor: Theme.of(context).colorScheme.onSurface),
+        child: Text(content));
+  }
 }
--- DeyGo/lib/components/inputs/inputs.dart
+++ DeyGo/lib/components/inputs/inputs.dart
@@ -1,79 +1,135 @@
 // import 'package:deygo/presentation/custom_icons_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-Widget phoneInput({required BuildContext context}) {
-  return TextFormField(
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.phone),
-      border: const OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(Radius.circular(10))),
-      hintText: 'Phone Number',
-      contentPadding: const EdgeInsets.all(25),
-      fillColor: Theme.of(context).colorScheme.surface,
-      // prefixIconColor: Theme.of(context).colorScheme.onSurface,
-      focusColor: Theme.of(context).colorScheme.primary,
-      filled: true,
-      errorBorder: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.error)),
-      focusedBorder: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary)),
-    ),
-    textAlign: TextAlign.left,
-    keyboardType: TextInputType.phone,
-  );
+abstract class InputBase extends StatelessWidget {
+  const InputBase({super.key, required this.controller});
+
+  final TextEditingController controller;
 }
 
-Widget passwordInput({required BuildContext context}) {
-  return TextFormField(
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.lock_outline),
-      suffixIcon: const Icon(Icons.remove_red_eye),
-      border: const OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(Radius.circular(10))),
-      hintText: 'Password',
-      contentPadding: const EdgeInsets.all(25),
-      fillColor: Theme.of(context).colorScheme.surface,
-      // prefixIconColor: Theme.of(context).colorScheme.onSurface,
-      filled: true,
-      focusColor: Theme.of(context).colorScheme.primary,
-      errorBorder: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.error)),
-      focusedBorder: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary)),
-    ),
-    textAlign: TextAlign.left,
-    keyboardType: TextInputType.text,
-    obscureText: true,
-  );
+class PhoneInput extends InputBase {
+  const PhoneInput({super.key, required super.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintStyle: GoogleFonts.urbanist(),
+        prefixIcon: const Icon(Icons.phone),
+        border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        hintText: 'Phone Number',
+        contentPadding: const EdgeInsets.all(25),
+        fillColor: Theme.of(context).colorScheme.surface,
+        // prefixIconColor: Theme.of(context).colorScheme.onSurface,
+        focusColor: Theme.of(context).colorScheme.primary,
+        filled: true,
+        errorBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Theme.of(context).colorScheme.error)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.primary)),
+      ),
+      textAlign: TextAlign.left,
+      keyboardType: TextInputType.phone,
+    );
+  }
 }
 
-Widget textInput(
-    {required BuildContext context, String hintText = 'Enter Text'}) {
-  return TextFormField(
-    decoration: InputDecoration(
-      border: const OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(Radius.circular(10))),
-      hintText: hintText,
-      contentPadding: const EdgeInsets.all(25),
-      fillColor: Theme.of(context).colorScheme.surface,
-      // prefixIconColor: Theme.of(context).colorScheme.onSurface,
-      filled: true,
-      focusColor: Theme.of(context).colorScheme.primary,
-      errorBorder: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.error)),
-      focusedBorder: OutlineInputBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary)),
-    ),
-    textAlign: TextAlign.left,
-    keyboardType: TextInputType.text,
-  );
+class PasswordInput extends StatefulWidget {
+  final TextEditingController controller;
+
+  const PasswordInput({super.key, required this.controller});
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<PasswordInput> createState() => _PasswordInputState(controller);
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  final TextEditingController controller;
+
+  _PasswordInputState(this.controller);
+
+  bool showPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintStyle: GoogleFonts.urbanist(),
+        prefixIcon: const Icon(Icons.lock_outline),
+        suffixIcon: IconButton(
+          icon: Icon(showPassword
+              ? Icons.remove_red_eye_outlined
+              : Icons.remove_red_eye),
+          onPressed: () {
+            setState(() {
+              showPassword = !showPassword;
+            });
+          },
+        ),
+        border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        hintText: 'Password',
+        contentPadding: const EdgeInsets.all(25),
+        fillColor: Theme.of(context).colorScheme.surface,
+        // prefixIconColor: Theme.of(context).colorScheme.onSurface,
+        filled: true,
+        focusColor: Theme.of(context).colorScheme.primary,
+        errorBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Theme.of(context).colorScheme.error)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.primary)),
+      ),
+      textAlign: TextAlign.left,
+      keyboardType: TextInputType.text,
+      obscureText: !showPassword,
+    );
+  }
+}
+
+class TextInput extends InputBase {
+  const TextInput(
+      {this.hintText = 'Enter Text', super.key, required super.controller});
+
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintStyle: GoogleFonts.urbanist(),
+        border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        hintText: hintText,
+        contentPadding: const EdgeInsets.all(25),
+        fillColor: Theme.of(context).colorScheme.surface,
+        // prefixIconColor: Theme.of(context).colorScheme.onSurface,
+        filled: true,
+        focusColor: Theme.of(context).colorScheme.primary,
+        errorBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Theme.of(context).colorScheme.error)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.primary)),
+      ),
+      textAlign: TextAlign.left,
+      keyboardType: TextInputType.text,
+    );
+  }
 }
