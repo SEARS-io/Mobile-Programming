6c7cba10ba30be9ab5dd78c8ede5499c5d46d0d5
Blanco237 <asongrandy9@gmail.com>
2023-06-15 23:25:32 +0100

    Added Constants

---
+++ DeyGo/lib/constants/image_strings.dart
@@ -0,0 +1,12 @@
+// Splash Images
+const String sBackground = 'assets/images/background.png';
+
+// Onboarding Images
+const String oImageOne = 'assets/images/onboarding_images/onboarding1.png';
+const String oImageTwo = 'assets/images/onboarding_images/onboarding2.png';
+const String oImageThree = 'assets/images/onboarding_images/onboarding3.png';
+
+// Authentication Images
+const String aImageGetIn = 'assets/images/auth_images/welcome.png';
+const String aImageGoogle = 'assets/images/auth_images/google.png';
+const String aImageFb = 'assets/images/auth_images/facebook.png';
---
+++ DeyGo/lib/constants/text_strings.dart
@@ -0,0 +1,24 @@
+// Splash Screen Text
+const String sWelcome = 'Welcome To';
+const String sAppName = 'DeyGo';
+const String sAppTagLine =
+    'We make your day great by giving you the best possible experience';
+
+// Onboarding Screen Text
+const String oMessageOne = 'We provide taxi services for you';
+const String oMessageTwo = 'Your satisfaction is our number one priority';
+const String oMessageThree = 'Let’s make your day great with DeyGo right now!';
+
+// Authentication Screen Text
+const String aGetIn = 'Let’s get in';
+const String aFbContinue = 'Continue with Facebook';
+const String aGoogleContinue = 'Continue with Google';
+const String aFacebook = 'Facebook';
+const String aGoogle = 'Google';
+const String aNoAccount = 'Don’t have an account?';
+const String aAlreadyAccount = 'Already have an account?';
+const String aSignIn = 'Sign in';
+const String aSignUp = 'Sign up';
+const String aSignInPassword = 'Sign in with password';
+const String aContinueWith = 'or continue with';
+const String aOr = 'or';
