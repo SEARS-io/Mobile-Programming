7ca2481d0c17b28f696ad234b2635c5c433bcc45
Blanco237 <asongrandy9@gmail.com>
2023-06-15 23:27:30 +0100

    Updates

--- DeyGo/lib/main.dart
+++ DeyGo/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:deygo/components/inputs/inputs.dart';
+import 'package:deygo/screens/authentication/get_in_screen.dart';
+import 'package:deygo/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'Theme/theme.dart';
-import 'components/buttons/primary.button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,69 +15,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'DeyGo',
       theme: AppTheme.theme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-            child: Column(
-          children: [
-            SizedBox.fromSize(size: const Size.fromHeight(20)),
-            SizedBox(
-              width: 350,
-              child: primaryButton(
-                  content: 'Dey Go',
-                  onPressed: () {},
-                  context: context,
-                  disabled: false,
-                  hasShadow: true),
-            ),
-            SizedBox.fromSize(size: const Size.fromHeight(20)),
-            phoneInput(context: context),
-            SizedBox.fromSize(size: const Size.fromHeight(20)),
-            passwordInput(context: context),
-            SizedBox.fromSize(size: const Size.fromHeight(20)),
-            textInput(context: context, hintText: 'Email')
-          ],
-        )),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        foregroundColor: Theme.of(context).primaryColor,
-        child: const Icon(Icons.cabin),
-      ),
+      home: SplashScreen(),
     );
   }
 }
